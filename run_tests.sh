96b64b6e184789d0e8196462b5b44d957060a0f9
Daniel de Vries <daniel.devries@darcorp.com>
2019-09-11 12:25:10 -0500

    Added run_tests.sh script, which should wait for db to come online

---
+++ run_tests.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+docker-compose build
+docker-compose up -d
+
+while ! docker-compose exec runs-db psql --username=postgres -c 'SELECT 1'; do
+  echo 'Waiting for postgres...'
+  sleep 1;
+done;
+
+docker-compose exec runs python manage.py test
+docker-compose exec runs flake8 project
+
+docker-compose down
