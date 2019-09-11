02e829387a030d023f42939e5e8513be4ce3521c
Daniel de Vries <daniel.devries@darcorp.com>
2019-09-11 10:17:54 -0500

    Added test coverage CLI command to runs service

--- services/runs/manage.py
+++ services/runs/manage.py
@@ -1,3 +1,4 @@
+import coverage
 import sys
 import unittest
 
@@ -5,6 +6,16 @@ from flask.cli import FlaskGroup
 
 from project import create_app, db
 from project.api.models import Run
+
+COV = coverage.coverage(
+    branch=True,
+    include='project/*',
+    omit=[
+        'project/tests/*',
+        'project/config.py',
+    ]
+)
+COV.start()
 
 
 app = create_app()
@@ -32,6 +43,22 @@ def test():
     tests = unittest.TestLoader().discover('project/tests', pattern='test*.py')
     result = unittest.TextTestRunner(verbosity=2).run(tests)
     if result.wasSuccessful():
+        return 0
+    sys.exit(result)
+
+
+@cli.command()
+def cov():
+    """Runs the unit tests with coverage."""
+    tests = unittest.TestLoader().discover('project/tests')
+    result = unittest.TextTestRunner(verbosity=2).run(tests)
+    if result.wasSuccessful:
+        COV.stop()
+        COV.save()
+        print('Coverage Summary:')
+        COV.report()
+        COV.html_report()
+        COV.erase()
         return 0
     sys.exit(result)
 
