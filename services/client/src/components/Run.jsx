a35342d5f6820a425e0e3ac33356898d6e3bb0d1
Daniel de Vries <daniel.devries@darcorp.com>
2019-09-24 16:10:28 -0500

    Styling improvement

---
+++ services/client/src/components/Run.jsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import {
+  Button,
+  Card,
+  CardActions,
+  CardActionArea,
+  CardContent,
+  CardMedia,
+  makeStyles,
+  Typography,
+} from "@material-ui/core";
+
+const useStyles = makeStyles(theme => ({
+  card: {
+    maxWidth: 345,
+    margin: theme.spacing(2),
+  },
+  media: {
+    height: 50,
+  },
+}));
+
+export default function Run(props) {
+  const classes = useStyles();
+  const { run } = props;
+
+  return (
+    <Card className={classes.card}>
+      <CardActionArea>
+        <CardMedia
+          className={classes.media}
+          // image="/static/images/cards/contemplative-reptile.jpg"
+          title="Optimization Run"
+          style={
+            {
+              backgroundColor: run.status === 0 ? 'lightblue' : (
+                run.status === 1 ? 'orange' : (
+                  run.status === 2 ? 'lightgreen' : 'red'
+                )
+              )
+            }
+          }
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            Optimization Run
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            Run status:
+            {run.status === 0 && " In Queue"}
+            {run.status === 1 && " Running..."}
+            {run.status === 2 && " Completed..."}
+            {![0, 1, 2].includes(run.status) && " Failed"}
+            <br/>
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small" color="primary">
+          Details
+        </Button>
+      </CardActions>
+    </Card>
+  );
+}
--- services/client/src/index.js
+++ services/client/src/index.js
@@ -1,14 +1,66 @@
-import React from 'react';
+import React, {Component, useEffect} from 'react';
 import ReactDOM from 'react-dom';
-import "./index.scss"
+import {
+  AppBar,
+  Fab,
+  makeStyles,
+  Paper,
+  Toolbar,
+  Typography, Container,
+  Grid,
+} from "@material-ui/core";
+import AddIcon from "@material-ui/icons/Add";
+import KeyboardArrowUpIcon from '@material-ui/icons/KeyboardArrowUp';
+import axios from "axios";
 
 import AddRun from "./components/AddRun";
-import RunsList from "./components/RunsList";
-import MyCollapsible from "./components/MyCollapsible";
-import {Button} from "@material-ui/core";
+import Run from "./components/Run";
+import ScrollTop from "./components/ScrollTop";
+import "./index.scss"
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    paddingBottom: theme.spacing(3),
+  },
+  fab: {
+    position: 'absolute',
+    zIndex: 1,
+    top: 30,
+    left: 'auto',
+    right: 30,
+    margin: '0 auto',
+  },
+  appBar: {
+    top: 0,
+    bottom: 'auto',
+  },
+  paper: {
+    marginTop: -50,
+    paddingTop: 50,
+    paddingBottom: theme.spacing(3),
+  }
+}));
 
 const App = (props) => {
+  const classes = useStyles();
   const [addRunOpen, setAddRunOpen] = React.useState(false);
+  const [runs, setRuns] = React.useState([]);
+
+  useEffect(() => {
+    const getRuns = () => {
+      axios.get(`${process.env.REACT_APP_RUNS_SERVICE_URL}/runs`)
+      .then((res) => {
+        setRuns(res.data.data.runs);
+      })
+      .catch((err) => { console.log(err); });
+    };
+
+    const interval = setInterval(getRuns, 1000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  });
 
   const handleClickOpen = () => {
     setAddRunOpen(true);
@@ -20,21 +72,39 @@ const App = (props) => {
 
   return (
     <section className="section">
-      <div className="container">
-        <div className="columns">
-          <div className="column is-two-thirds">
-            <Button
-              variant="contained"
-              color="primary"
-              onClick={handleClickOpen}
-            >Submit New Run</Button>
-            <AddRun open={addRunOpen} onClose={handleClose}/>
-            <MyCollapsible titleClassName="title is-2" title="Run List">
-              <RunsList/>
-            </MyCollapsible>
-          </div>
-        </div>
-      </div>
+      <AddRun open={addRunOpen} onClose={handleClose}/>
+      <AppBar className={classes.appBar}>
+        <Toolbar>
+          <Typography variant="h6">Optimization Runs</Typography>
+          <Fab
+            color="secondary"
+            aria-label="add"
+            className={classes.fab}
+            onClick={handleClickOpen}
+          >
+            <AddIcon/>
+          </Fab>
+        </Toolbar>
+      </AppBar>
+      <Toolbar id="back-to-top-anchor" />
+      <Paper className={classes.paper}>
+        <Grid
+          container
+          justify="center"
+          alignItems="center"
+        >
+          {runs.map((run) => (
+            <Grid item>
+              <Run run={run} />
+            </Grid>
+          ))}
+        </Grid>
+      </Paper>
+      <ScrollTop {...props}>
+        <Fab color="secondary" size="small" aria-label="scroll back to top">
+          <KeyboardArrowUpIcon />
+        </Fab>
+      </ScrollTop>
     </section>
   );
 };
