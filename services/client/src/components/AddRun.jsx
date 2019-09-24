f71af91c29294b294fac869c6903381f527c553e
Daniel de Vries <daniel.devries@darcorp.com>
2019-09-24 16:40:32 -0500

    Improved add run dialog

--- services/client/src/components/AddRun.jsx
+++ services/client/src/components/AddRun.jsx
@@ -4,14 +4,15 @@ import axios from "axios";
 import { makeStyles } from '@material-ui/core/styles';
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 import {
+  Box,
   Button,
   ButtonGroup,
   Container,
-  Dialog, DialogTitle,
+  Dialog, DialogActions, DialogContent, DialogTitle,
   ExpansionPanel,
   ExpansionPanelDetails,
   ExpansionPanelSummary,
-  FormControlLabel,
+  FormControlLabel, Paper,
   Switch,
   TextField,
   Typography
@@ -27,13 +28,17 @@ const useStyles = makeStyles(theme => ({
     marginLeft: theme.spacing(1),
     marginRight: theme.spacing(1),
   },
-  button: {
-    marginTop: theme.spacing(1),
+  buttonGroup: {
+    margin: theme.spacing(1),
   },
   heading: {
     fontSize: theme.typography.pxToRem(15),
     fontWeight: theme.typography.fontWeightRegular,
   },
+  paper: {
+    padding: theme.spacing(2),
+    margin: theme.spacing(2),
+  }
 }));
 
 export default function AddRun(props) {
@@ -55,7 +60,11 @@ export default function AddRun(props) {
   });
 
   const handleChange = name => event => {
-    setValues({ ...values, [name]: event.target.value });
+    if (['fix_te', 'constrain_thickness', 'constrain_area', 'constrain_moment', 'report'].includes(name)) {
+      setValues({ ...values, [name]: !values[name]})
+    } else {
+      setValues({...values, [name]: event.target.value});
+    }
   };
 
   const handleClose = () => {
@@ -90,236 +99,208 @@ export default function AddRun(props) {
     <Dialog
       className={classes.dialog}
       open={open}
+      onClose={handleClose}
     >
       <DialogTitle>Submit New Run</DialogTitle>
       <form onSubmit={(event) => addRun(event)}>
-        <ExpansionPanel>
-          <ExpansionPanelSummary
-            expandIcon={<ExpandMoreIcon />}
-            aria-controls="panel1a-content"
-            id="panel1a-header"
+        <DialogContent>
+          <Paper
+            container
+            maxWidth="xs"
+            className={classes.paper}
           >
-            <Typography className={classes.heading}>Basic Problem Setup</Typography>
-          </ExpansionPanelSummary>
-          <ExpansionPanelDetails>
-            <Container maxWidth="xs">
-              <TextField
-                label="Design Lift Coefficient"
-                name="cl"
-                value={values.cl}
-                onChange={handleChange('cl')}
-                type="number"
-                inputProps={{
-                  step: 0.001,
-                }}
-                className={classes.textField}
-                InputLabelProps={{
-                  shrink: true,
-                }}
-                margin="normal"
-              />
-              <br/>
-              <FormControlLabel control={
-                <Switch
-                  checked={values.fix_te}
-                  onChange={handleChange('fix_te')}
-                  value="fix_te"
-                  name="fix_te"
-                  inputProps={{ 'aria-label': 'primary checkbox' }}
-                />} label="Fix TE Thickness?" //labelPlacement="top"
-              />
-              <br/>
-              <FormControlLabel control={
-                <Switch
-                  checked={values.constrain_thickness}
-                  onChange={handleChange('constrain_thickness')}
-                  name="constrain_thickness"
-                  inputProps={{ 'aria-label': 'primary checkbox' }}
-                />} label="Constrain Thickness?" //labelPlacement="top"
-              />
-              <br/>
-              <FormControlLabel control={
-                <Switch
-                  checked={values.constrain_area}
-                  onChange={handleChange('constrain_area')}
-                  name="constrain_area"
-                  inputProps={{ 'aria-label': 'primary checkbox' }}
-                />} label="Constrain Area?" //labelPlacement="top"
-              />
-              <br/>
-              <FormControlLabel control={
-                <Switch
-                  checked={values.constrain_moment}
-                  onChange={handleChange('constrain_moment')}
-                  name="constrain_moment"
-                  inputProps={{ 'aria-label': 'primary checkbox' }}
-                />} label="Constrain Moment?" //labelPlacement="top"
-              />
-            </Container>
-          </ExpansionPanelDetails>
-        </ExpansionPanel>
-
-        <ExpansionPanel>
-          <ExpansionPanelSummary
-            expandIcon={<ExpandMoreIcon />}
-            aria-controls="panel1a-content"
-            id="panel1a-header"
+            <Typography variant="h7">Basic Problem Setup</Typography>
+            <br/>
+            <TextField
+              label="Design Lift Coefficient"
+              name="cl"
+              value={values.cl}
+              onChange={handleChange('cl')}
+              type="number"
+              inputProps={{
+                step: 0.001,
+              }}
+              className={classes.textField}
+              InputLabelProps={{
+                shrink: true,
+              }}
+              margin="normal"
+            />
+            <br/>
+            <FormControlLabel control={
+              <Switch
+                checked={values.fix_te}
+                onChange={handleChange('fix_te')}
+                value="fix_te"
+                name="fix_te"
+                inputProps={{ 'aria-label': 'primary checkbox' }}
+              />} label="Fix TE Thickness?" //labelPlacement="top"
+            />
+            <br/>
+            <FormControlLabel control={
+              <Switch
+                checked={values.constrain_thickness}
+                onChange={handleChange('constrain_thickness')}
+                name="constrain_thickness"
+                inputProps={{ 'aria-label': 'primary checkbox' }}
+              />} label="Constrain Thickness?" //labelPlacement="top"
+            />
+            <br/>
+            <FormControlLabel control={
+              <Switch
+                checked={values.constrain_area}
+                onChange={handleChange('constrain_area')}
+                name="constrain_area"
+                inputProps={{ 'aria-label': 'primary checkbox' }}
+              />} label="Constrain Area?" //labelPlacement="top"
+            />
+            <br/>
+            <FormControlLabel control={
+              <Switch
+                checked={values.constrain_moment}
+                onChange={handleChange('constrain_moment')}
+                name="constrain_moment"
+                inputProps={{ 'aria-label': 'primary checkbox' }}
+              />} label="Constrain Moment?" //labelPlacement="top"
+            />
+          </Paper>
+          <Paper
+            container
+            maxWidth="xs"
+            className={classes.paper}
           >
-            <Typography className={classes.heading}>Number of Design Variables</Typography>
-          </ExpansionPanelSummary>
-          <ExpansionPanelDetails>
-            <Container maxWidth="xs">
-              <TextField
-                label="Mean Chord Line"
-                name="n_c"
-                value={values.n_c}
-                onChange={handleChange('n_c')}
-                type="number"
-                inputProps={{
-                  min: 1,
-                }}
-                className={classes.textField}
-                InputLabelProps={{
-                  shrink: true,
-                }}
-                margin="normal"
-              />
-              <br/>
-              <TextField
-                label="Thickness Distribution"
-                name="n_t"
-                value={values.n_t}
-                onChange={handleChange('n_t')}
-                type="number"
-                inputProps={{
-                  min: 1,
-                }}
-                className={classes.textField}
-                InputLabelProps={{
-                  shrink: true,
-                }}
-                margin="normal"
-              />
-              <br/>
-              <Typography component="i">
-                It is recommended to use between 3 and 6 for each.
-              </Typography>
-            </Container>
-          </ExpansionPanelDetails>
-        </ExpansionPanel>
-
-        <ExpansionPanel>
-          <ExpansionPanelSummary
-            expandIcon={<ExpandMoreIcon />}
-            aria-controls="panel1a-content"
-            id="panel1a-header"
+            <Typography variant="h7">Number of Design Variables</Typography>
+            <br/>
+            <TextField
+              label="Mean Chord Line"
+              name="n_c"
+              value={values.n_c}
+              onChange={handleChange('n_c')}
+              type="number"
+              inputProps={{
+                min: 1,
+              }}
+              className={classes.textField}
+              InputLabelProps={{
+                shrink: true,
+              }}
+              margin="normal"
+            />
+            <br/>
+            <TextField
+              label="Thickness Distribution"
+              name="n_t"
+              value={values.n_t}
+              onChange={handleChange('n_t')}
+              type="number"
+              inputProps={{
+                min: 1,
+              }}
+              className={classes.textField}
+              InputLabelProps={{
+                shrink: true,
+              }}
+              margin="normal"
+            />
+          </Paper>
+          <Paper
+            container
+            maxWidth="xs"
+            className={classes.paper}
           >
-            <Typography className={classes.heading}>Termination Settings</Typography>
-          </ExpansionPanelSummary>
-          <ExpansionPanelDetails>
-            <Container maxWidth="xs">
-              <TextField
-                label="Number of Generations"
-                name="gen"
-                value={values.gen}
-                onChange={handleChange('gen')}
-                type="number"
-                inputProps={{
-                  min: 0,
-                }}
-                className={classes.textField}
-                InputLabelProps={{
-                  shrink: true,
-                }}
-                margin="normal"
-              />
-              <br/>
-              <TextField
-                label="Design Vector Tolerance"
-                name="tolx"
-                value={values.tolx}
-                onChange={handleChange('tolx')}
-                type="text"
-                inputProps={{
-                  pattern: "[+-]?([1-9]\\d*|0)?(\\.\\d*)?([Ee][+-]?\\d+)?",
-                }}
-                className={classes.textField}
-                InputLabelProps={{
-                  shrink: true,
-                }}
-                margin="normal"
-              />
-              <br/>
-              <TextField
-                label="Objective Function Tolerance"
-                name="tolf"
-                value={values.tolf}
-                onChange={handleChange('tolf')}
-                type="text"
-                inputProps={{
-                  pattern: "[+-]?([1-9]\\d*|0)?(\\.\\d*)?([Ee][+-]?\\d+)?",
-                }}
-                className={classes.textField}
-                InputLabelProps={{
-                  shrink: true,
-                }}
-                margin="normal"
-              />
-            </Container>
-          </ExpansionPanelDetails>
-        </ExpansionPanel>
-
-        <ExpansionPanel>
-          <ExpansionPanelSummary
-            expandIcon={<ExpandMoreIcon />}
-            aria-controls="panel1a-content"
-            id="panel1a-header"
+            <Typography variant="h7">Termination Settings</Typography>
+            <br/>
+            <TextField
+              label="Number of Generations"
+              name="gen"
+              value={values.gen}
+              onChange={handleChange('gen')}
+              type="number"
+              inputProps={{
+                min: 0,
+              }}
+              className={classes.textField}
+              InputLabelProps={{
+                shrink: true,
+              }}
+              margin="normal"
+            />
+            <br/>
+            <TextField
+              label="Design Vector Tolerance"
+              name="tolx"
+              value={values.tolx}
+              onChange={handleChange('tolx')}
+              type="text"
+              inputProps={{
+                pattern: "[+-]?([1-9]\\d*|0)?(\\.\\d*)?([Ee][+-]?\\d+)?",
+              }}
+              className={classes.textField}
+              InputLabelProps={{
+                shrink: true,
+              }}
+              margin="normal"
+            />
+            <br/>
+            <TextField
+              label="Objective Function Tolerance"
+              name="tolf"
+              value={values.tolf}
+              onChange={handleChange('tolf')}
+              type="text"
+              inputProps={{
+                pattern: "[+-]?([1-9]\\d*|0)?(\\.\\d*)?([Ee][+-]?\\d+)?",
+              }}
+              className={classes.textField}
+              InputLabelProps={{
+                shrink: true,
+              }}
+              margin="normal"
+            />
+          </Paper>
+          <Paper
+            container
+            maxWidth="xs"
+            className={classes.paper}
           >
-            <Typography className={classes.heading}>Miscellaneous Settings</Typography>
-          </ExpansionPanelSummary>
-          <ExpansionPanelDetails>
-            <Container maxWidth="xs">
-              <TextField
-                label="Number of Processors"
-                name="n_proc"
-                value={values.n_proc}
-                onChange={handleChange('n_proc')}
-                type="number"
-                inputProps={{
-                  min: 1,
-                }}
-                className={classes.textField}
-                InputLabelProps={{
-                  shrink: true,
-                }}
-                margin="normal"
-              />
-              <br/>
-              <FormControlLabel control={
-                <Switch
-                  checked={values.report}
-                  onChange={handleChange('report')}
-                  value="report"
-                  name="report"
-                  inputProps={{ 'aria-label': 'primary checkbox' }}
-                />} label="Report Results via Email?" //labelPlacement="top"
-              />
-            </Container>
-          </ExpansionPanelDetails>
-        </ExpansionPanel>
-
-        <ButtonGroup fullWidth aria-label="full width outlined button group">
+            <Typography variant="h7">Miscellaneous</Typography>
+            <TextField
+              label="Number of Processors"
+              name="n_proc"
+              value={values.n_proc}
+              onChange={handleChange('n_proc')}
+              type="number"
+              inputProps={{
+                min: 1,
+              }}
+              className={classes.textField}
+              InputLabelProps={{
+                shrink: true,
+              }}
+              margin="normal"
+            />
+            <br/>
+            <FormControlLabel control={
+              <Switch
+                checked={values.report}
+                onChange={handleChange('report')}
+                value="report"
+                name="report"
+                inputProps={{ 'aria-label': 'primary checkbox' }}
+              />} label="Report Results via Email?" //labelPlacement="top"
+            />
+          </Paper>
+        </DialogContent>
+        <DialogActions>
           <Button
-            variant="contained"
             color="primary"
             onClick={handleClose}
           >Cancel</Button>
           <Button
             type="submit"
-            variant="contained"
             color="primary"
           >Submit</Button>
-        </ButtonGroup>
+        </DialogActions>
       </form>
     </Dialog>
   );
